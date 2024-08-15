9a023cf85b02679669c8ab40e327602054f12d4d
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-08-15 06:30:22 -0400

    Create saturn_cloud.sh

---
+++ saturn_cloud.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip install celldega==0.3.0 --user
+
+# Enable nbextension for classic Jupyter Notebook
+jupyter nbextension install --py anywidget --user
+jupyter nbextension enable --py anywidget --user
+
+# Enable nbextension for classic Jupyter Notebook
+jupyter nbextension install --py anywidget --user
+jupyter nbextension enable --py anywidget --user
