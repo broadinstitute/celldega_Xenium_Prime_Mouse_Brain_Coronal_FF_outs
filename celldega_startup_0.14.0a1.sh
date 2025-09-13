4fe76d9fb6c98f9783ff929d8491488d85ede79f
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-09-12 22:52:17 -0400

    Create celldega_startup_0.14.0a1.sh

---
+++ celldega_startup_0.14.0a1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo -E -u jupyter /opt/conda/bin/pip install anywidget  --user
+sudo -E -u jupyter /opt/conda/bin/pip install celldega==0.14.0a1  --user
+
+# Enable nbextension for classic Jupyter Notebook
+jupyter nbextension install --py anywidget --user
+jupyter nbextension enable --py anywidget --user
